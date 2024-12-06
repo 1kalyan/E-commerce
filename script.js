ab9070df4d196b7de82edbe0ed456659e3728d5e
1kalyan <118203520+1kalyan@users.noreply.github.com>
2024-12-06 09:26:14 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function for navigating to the Home page
+document.getElementById("homeLink").addEventListener("click", function (e) {
+    e.preventDefault(); // Prevent the default anchor behavior
+    window.location.href = "/index.html"; // Redirect to Home page
+});
